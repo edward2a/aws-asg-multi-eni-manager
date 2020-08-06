c4b09b837794e930888d7c7ea1312970072b9db2
Eduardo A. Paris Penas <edward2a@gmail.com>
2020-08-06 03:43:46 +0100

    some reformatting to my personal preference

--- lambda/asg-eni-manager.py
+++ lambda/asg-eni-manager.py
@@ -9,43 +9,58 @@ asg_client = boto3.client('autoscaling')
 
 
 def lambda_handler(event, context):
+
     if event["detail-type"] == "EC2 Instance-launch Lifecycle Action":
+
         instance_id = event['detail']['EC2InstanceId']
-        LifecycleHookName=event['detail']['LifecycleHookName']
-        AutoScalingGroupName=event['detail']['AutoScalingGroupName']
+        LifecycleHookName = event['detail']['LifecycleHookName']
+        AutoScalingGroupName = event['detail']['AutoScalingGroupName']
         subnet_id = get_subnet_id(instance_id)
         interface_id = create_interface(subnet_id)
         attachment = attach_interface(interface_id, instance_id)
+
         if not interface_id:
-            complete_lifecycle_action_failure(LifecycleHookName,AutoScalingGroupName,instance_id)
+            complete_lifecycle_action_failure(
+                LifecycleHookName, AutoScalingGroupName, instance_id)
+
         elif not attachment:
-            complete_lifecycle_action_failure(LifecycleHookName,AutoScalingGroupName,instance_id)
+            complete_lifecycle_action_failure(
+                LifecycleHookName, AutoScalingGroupName, instance_id)
             delete_interface(interface_id)
+
         else:
-            complete_lifecycle_action_success(LifecycleHookName,AutoScalingGroupName,instance_id)
-       
-        
+            complete_lifecycle_action_success(
+                LifecycleHookName, AutoScalingGroupName, instance_id)
+
+
 def get_subnet_id(instance_id):
+
     try:
         result = ec2_client.describe_instances(InstanceIds=[instance_id])
         vpc_subnet_id = result['Reservations'][0]['Instances'][0]['SubnetId']
         log("Subnet id: {}".format(vpc_subnet_id))
 
     except botocore.exceptions.ClientError as e:
-        log("Error describing the instance {}: {}".format(instance_id, e.response['Error']))
+        log("Error describing the instance {}: {}".format(
+            instance_id, e.response['Error']))
+
         vpc_subnet_id = None
 
     return vpc_subnet_id
 
 
 def create_interface(subnet_id):
+
     network_interface_id = None
 
     if subnet_id:
         try:
-            network_interface = ec2_client.create_network_interface(SubnetId=subnet_id)
+            network_interface = ec2_client.create_network_interface(
+                SubnetId=subnet_id)
             network_interface_id = network_interface['NetworkInterface']['NetworkInterfaceId']
+
             log("Created network interface: {}".format(network_interface_id))
+
         except botocore.exceptions.ClientError as e:
             log("Error creating network interface: {}".format(e.response['Error']))
 
@@ -71,31 +86,40 @@ def attach_interface(network_interface_id, instance_id):
 
 
 def delete_interface(network_interface_id):
+
     try:
         ec2_client.delete_network_interface(
-            NetworkInterfaceId=network_interface_id
-        )
-        log("Deleted network interface: {}".format(network_interface_id))
+            NetworkInterfaceId=network_interface_id)
+
+        log("Deleted network interface: {}".format(
+            network_interface_id))
+
         return True
 
     except botocore.exceptions.ClientError as e:
-        log("Error deleting interface {}: {}".format(network_interface_id, e.response['Error']))
-        
-        
-def complete_lifecycle_action_success(hookname,groupname,instance_id):
+        log("Error deleting interface {}: {}".format(
+            network_interface_id, e.response['Error']))
+
+
+def complete_lifecycle_action_success(hookname, groupname, instance_id):
+
     try:
         asg_client.complete_lifecycle_action(
-                LifecycleHookName=hookname,
-                AutoScalingGroupName=groupname,
-                InstanceId=instance_id,
-                LifecycleActionResult='CONTINUE'
-            )
+            LifecycleHookName=hookname,
+            AutoScalingGroupName=groupname,
+            InstanceId=instance_id,
+            LifecycleActionResult='CONTINUE')
+
         log("Lifecycle hook CONTINUEd for: {}".format(instance_id))
+
     except botocore.exceptions.ClientError as e:
-            log("Error completing life cycle hook for instance {}: {}".format(instance_id, e.response['Error']))
-            log('{"Error": "1"}')    
-            
-def complete_lifecycle_action_failure(hookname,groupname,instance_id):
+        log("Error completing life cycle hook for instance {}: {}".format(
+            instance_id, e.response['Error']))
+        log('{"Error": "1"}')
+
+
+def complete_lifecycle_action_failure(hookname, groupname, instance_id):
+
     try:
         asg_client.complete_lifecycle_action(
                 LifecycleHookName=hookname,
@@ -104,10 +128,13 @@ def complete_lifecycle_action_failure(hookname,groupname,instance_id):
                 LifecycleActionResult='ABANDON'
             )
         log("Lifecycle hook ABANDONed for: {}".format(instance_id))
+
     except botocore.exceptions.ClientError as e:
-            log("Error completing life cycle hook for instance {}: {}".format(instance_id, e.response['Error']))
-            log('{"Error": "1"}')    
-    
+        log("Error completing life cycle hook for instance {}: {}".format(
+            instance_id, e.response['Error']))
+        log('{"Error": "1"}')
+
 
 def log(error):
     print('{}Z {}'.format(datetime.utcnow().isoformat(), error))
+
